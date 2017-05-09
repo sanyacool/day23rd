46eb1af655e48603f28381797eec2d2cf3c44a75
Aleksandr <sanyacool2010@mail.ru>
2017-05-10 02:48:34 +0800

    fix

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.get('/', function(req, res){
 
 var users = {}; 
 io.on('connection', function(client){
+	console.log('u ' + users);
 	client.emit('show other', users);
 	client.emit('user connected', client.id);
 	client.on('connection done', function(coordx, coordy, plcol, plrad){
+		console.log('c ' + client.id);
 		users[client.id] = {
 			x: coordx,
 			y: coordy,
@@ -23,11 +25,9 @@ io.on('connection', function(client){
 
 	client.emit('moving', client.id);
 	client.on('moving done', function(coordx, coordy){
-		users[client.id] = {
-			x: coordx,
-			y: coordy
-		}
-		client.broadcast.emit('user done', coordx, coordy, client.id)
+		users[client.id].x = coordx;
+		users[client.id].y = coordy;
+		client.broadcast.emit('moving done', coordx, coordy, client.id)
 	});
 	
 	client.on('disconnect', function(){
