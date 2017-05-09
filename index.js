8e5206f14c4f97a46a7ba5e60a42a0bb9a76e341
Aleksandr <sanyacool2010@mail.ru>
2017-05-10 01:51:31 +0800

    create

---
+++ index.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var users = {}; 
+io.on('connection', function(client){
+	client.emit('show other', users);
+	client.emit('user connected', client.id);
+	client.on('connection done', function(coordx, coordy, plcol, plrad){
+		users[client.id] = {
+			x: coordx,
+			y: coordy,
+			col: plcol,
+			rad: plrad
+		}
+		client.broadcast.emit('user done', coordx, coordy, plcol, plrad, client.id)
+	});
+
+	client.emit('moving', client.id);
+	client.on('moving done', function(coordx, coordy){
+		users[client.id] = {
+			x: coordx,
+			y: coordy
+		}
+		client.broadcast.emit('user done', coordx, coordy, client.id)
+	});
+	
+	client.on('disconnect', function(){
+		client.broadcast.emit('user disconnected', client.id);
+		delete users[client.id];
+	});
+
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
